98a28aac44865e8db7a8d23b2a011aaef7bc4911
Felix Winkler <Felix-Winkler96@web.de>
2026-05-20 15:21:24 +0200

    added skills section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { AboutMe } from './layout/about-me/about-me';
+import { Skillset } from './layout/skillset/skillset';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AboutMe],
+  imports: [RouterOutlet, Header, AboutMe, Skillset],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })

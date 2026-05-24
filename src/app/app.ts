2a99bdd018ac0e675d2f28090624f99f66890b9a
Felix Winkler <Felix-Winkler96@web.de>
2026-05-24 13:42:00 +0200

    start project section, added testimonial, contact sections + refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { AboutMe } from './layout/about-me/about-me';
 import { Skillset } from './layout/skillset/skillset';
+import { Projects } from './layout/projects/projects';
+import { Testimonials } from './layout/testimonials/testimonials';
+import { ContactMe } from './layout/contact-me/contact-me';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, AboutMe, Skillset],
+  imports: [RouterOutlet, Header, AboutMe, Skillset, Projects, Testimonials, ContactMe],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
---
+++ src/app/shared/components/blue-btn/blue-btn.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-blue-btn',
+  imports: [],
+  templateUrl: './blue-btn.html',
+  styleUrl: './blue-btn.scss',
+})
+export class BlueBtn {
+
+}

b3bfd5fabd5d60921cec629b8f4c2d1f469469b6
Felix Winkler <Felix-Winkler96@web.de>
2026-06-03 18:41:01 +0200

    changed root color names + blue button animation

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -3,14 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MobileNav } from '../../pages/mobile-nav/mobile-nav';
 import { BlueBtnComponent } from '../../shared/components/blue-btn/blue-btn';
 
+interface InteractiveLetter {
+  char: string;
+  hovered: boolean;
+}
+
 @Component({
   selector: 'app-header',
   imports: [BlueBtnComponent, CommonModule, MobileNav],
   templateUrl: './header.html',
   styleUrls: ['./header.scss'],
 })
+
 export class Header {
   @ViewChild('mobileNav') mobileNav?: ElementRef<HTMLDialogElement>;
+
+  headerTitle1: InteractiveLetter[] = 'Frontend'.split('').map((char) => ({ char, hovered: false }));
+  headerTitle2: InteractiveLetter[] = 'DEVELOPER'.split('').map((char) => ({ char, hovered: false }));
 
   constructor(private cd: ChangeDetectorRef) {}
 
@@ -22,5 +31,17 @@ export class Header {
 
   closeMobileMenu() {
     this.mobileNav?.nativeElement.close();
+  }
+
+  setLetterHover(letter: InteractiveLetter, hovered: boolean) {
+    letter.hovered = hovered;
+  }
+
+  getLetterDisplay(letter: InteractiveLetter) {
+    return letter.hovered ? this.toggleCase(letter.char) : letter.char;
+  }
+
+  private toggleCase(value: string) {
+    return value === value.toLowerCase() ? value.toUpperCase() : value.toLowerCase();
   }
 }

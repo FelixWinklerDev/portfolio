404e8d8f42545f20b6819be4c97ed5725bfd342f
Felix Winkler <Felix-Winkler96@web.de>
2026-07-19 14:29:14 +0200

    adjust responsive in: header, project, legal, testimonials

--- src/app/pages/legal/legal.ts
+++ src/app/pages/legal/legal.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { MobileNav } from '../mobile-nav/mobile-nav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-legal',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, MobileNav],
   templateUrl: './legal.html',
   styleUrl: './legal.scss',
 })
 export class Legal {
-  constructor(private router: Router) {}
+  @ViewChild('mobileNav') mobileNav?: ElementRef<HTMLDialogElement>;
+
+  constructor(private cd: ChangeDetectorRef, private router: Router) {}
+
+  openMobileMenu(event: MouseEvent) {
+    event.preventDefault();
+    this.cd.detectChanges();
+    this.mobileNav?.nativeElement.showModal();
+  }
+
+  closeMobileMenu() {
+    this.mobileNav?.nativeElement.close();
+  }
 
   goHome(): void {
     this.router.navigate(['/']);
